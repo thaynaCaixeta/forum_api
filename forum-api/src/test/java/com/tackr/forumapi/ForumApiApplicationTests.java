ee5bc79a5428b489ff834ea8b44839d3e6d7216a
thaynaCaixeta <thayna.caixeta@gmail.com>
2021-02-21 20:22:06 -0300

    Start project

---
+++ forum-api/src/test/java/com/tackr/forumapi/ForumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tackr.forumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
