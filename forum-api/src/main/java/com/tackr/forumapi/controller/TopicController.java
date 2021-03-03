bce334b68c6ccc42cbd5cb79ac20c61ed26b7327
Thayná Rocha <thayna.rocha@bancointer.com.br>
2021-03-03 17:04:31 -0300

    Implement default pagination

--- forum-api/src/main/java/com/tackr/forumapi/controller/TopicController.java
+++ forum-api/src/main/java/com/tackr/forumapi/controller/TopicController.java
@@ -1,7 +1,6 @@
 package com.tackr.forumapi.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -9,8 +8,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,7 +43,7 @@ public class TopicController {
 
 	@GetMapping
 	public Page<TopicDto> list(@RequestParam(required = false) String courseName,
-			Pageable pagination) {
+			@PageableDefault(sort = "id", direction = Direction.DESC) Pageable pagination) {
 				
 		if (courseName != null) {
 			Page<Topic> topics = topicRepository.findByCourseName(courseName, pagination);
