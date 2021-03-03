55788590e3f25052649f119c294633551c61a7fa
Thayná Rocha <thayna.rocha@bancointer.com.br>
2021-03-03 18:19:42 -0300

    Add spring security dependence

---
+++ forum-api/src/main/java/com/tackr/forumapi/config/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.tackr.forumapi.config.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
