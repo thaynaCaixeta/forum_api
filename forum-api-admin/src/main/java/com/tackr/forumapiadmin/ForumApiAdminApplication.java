149c00915bd1d4df54fe04bc1a884dfb3a726741
Thayná Rocha <thayna.rocha@bancointer.com.br>
2021-03-04 12:39:41 -0300

    Add spring admin client to api monitoring

---
+++ forum-api-admin/src/main/java/com/tackr/forumapiadmin/ForumApiAdminApplication.java
@@ -0,0 +1,17 @@
+package com.tackr.forumapiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class ForumApiAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApiAdminApplication.class, args);
+	}
+}
---
+++ forum-api-admin/src/test/java/com/tackr/forumapiadmin/ForumApiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tackr.forumapiadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApiAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
