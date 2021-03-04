0ccbe4d35d14bff2e8e74782ca72eb36ff4d4ceb
Thayná Rocha <thayna.rocha@bancointer.com.br>
2021-03-04 19:19:54 -0300

    Add first part of api documentation using spring fox swagger

---
+++ forum-api/src/main/java/com/tackr/forumapi/config/documentation/swagger/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package com.tackr.forumapi.config.documentation.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.tackr.forumapi.entity.User;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+	@Bean
+	public Docket forumApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.tackr.forumapi"))
+				.paths(PathSelectors.ant("/**"))
+				.build()
+				.ignoredParameterTypes(User.class);
+	}	
+}
