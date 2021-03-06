d7474102622b2fb019e67d98315dc84aa6d37ec9
thaynaCaixeta <thayna.caixeta@gmail.com>
2021-03-06 11:48:04 -0300

    Add dockerfile

--- forum-api-admin/src/main/java/com/tackr/forumapiadmin/ForumApiAdminApplication.java
+++ forum-api-admin/src/main/java/com/tackr/forumapiadmin/ForumApiAdminApplication.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
 public class ForumApiAdminApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApiAdminApplication.class, args);
 	}
--- forum-api/src/test/java/com/tackr/forumapi/ForumApiApplicationTests.java
+++ forum-api/src/test/java/com/tackr/forumapi/ForumApiApplicationTests.java
@@ -1,17 +1,10 @@
 package com.tackr.forumapi;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class ForumApiApplicationTests {
 
-	@Test
 	void contextLoads() {
-		Assert.assertTrue(true);
 	}
 }
